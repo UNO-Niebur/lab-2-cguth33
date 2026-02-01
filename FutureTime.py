81257af31ce428d2fb559c3fda79d6ad3fed9e50
cguth33 <carterguthrie@unomaha.edu>
2026-02-01 20:19:24 +0000

    Completed Lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -1,8 +1,8 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
-
+#Name:Carter Guthrie
+#Date:2/1/2025
+#Assignment:Lab 2- Future Time
+#Purpose: Calculate future time using modular arithmetic
 # datetime will allow us to access the system date and time.
 import datetime
 
@@ -17,13 +17,16 @@ def main():
   #TODO:
   #Ask user for hours
   #Ask user for minutes
-
+  extraHours = int(input("Enter hours to add: "))
+  extraMinutes = int(input("Enter minutes to add: "))
   #Calculate the time after the user-supplied time has passed.
-
+  totalMinutes = currentMinute + extraMinutes
+  futureMinute = totalMinutes % 60
   #Do not use any if statements in calculating the time.
-
+  carryOverHours = totalMinutes // 60
+  futureHour = (currentHour + extraHours + carryOverHours) % 24
   #Output the future time in standard format "HH:MM"
-
+  print(f"Future time is: {futureHour:02d}:{futureMinute:02d}")
 
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,20 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
-
+#Name:Carter Guthrie
+#Date:2/1/2025
+#Assignment:Lab 2
+#Purpose: Create A Random Response Generator
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
+  responses = ["Yes, definitely", "No, not a chance", "Ask again later", "It is certain", "Outlook not so good"]
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  input("Ask the Magic 8 Ball a question: ")
   #Answer question randomly with one of the options from your earlier list.
-
+  answer = random.choice(responses)
+  print(answer)
 
 if __name__ == '__main__':
   main()
